82a3b7c58eca3fa7aa30d2dfe7b82a0f4034ac00
Rafa Liang <rafaliang@rafas-mbp.dynamic.ucsd.edu>
2016-09-10 21:08:47 -0700

    Initial Commit

---
+++ Leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Leetcode
+//
+//  Created by Rafa Liang on 9/10/16.
+//  Copyright © 2016 Rafa Liang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
